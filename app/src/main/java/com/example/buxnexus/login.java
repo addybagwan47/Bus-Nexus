f9c6d5bb0055a9f15005e51ad58569d3307c79e2
addybagwan47 <149690533+addybagwan47@users.noreply.github.com>
2024-01-16 20:02:44 +0530

    Update login.java

--- app/src/main/java/com/example/buxnexus/login.java
+++ app/src/main/java/com/example/buxnexus/login.java
@@ -1,5 +1,7 @@
 package com.example.buxnexus;
 
+import androidx.activity.result.contract.ActivityResultContracts;
+import org.jetbrains.annotations.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
@@ -11,6 +13,13 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
 public class login extends AppCompatActivity {
 
     String[] items={"Student","Teacher","Driver"};
@@ -18,10 +27,34 @@ public class login extends AppCompatActivity {
     AppCompatButton btn;
     AutoCompleteTextView autoCompleteTextView;
     ArrayAdapter<String> adapterItems;
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btn = findViewById(R.id.gle_Btn_login);
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this,gso);
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+
+        if(acct!=null)
+        {
+            navigate();
+        }
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+
+
+
+
 
         adapterItems=new ArrayAdapter<String>(this,R.layout.list_item,items);
         autoCompleteTextView =findViewById(R.id.auto_complete_txt);
@@ -44,5 +77,33 @@ public class login extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+
+
+    void signIn() {
+        Intent signInIntent = gsc.getSignInIntent();
+        startActivityForResult(signInIntent,1000);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1000)
+        {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                task.getResult(ApiException.class);
+                navigate();
+            } catch (ApiException e) {
+                Toast.makeText(getApplicationContext(),"Something Went Wrong",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    void navigate(){
+        finish();
+        Intent intent = new Intent(login.this, HomePage.class);
+        startActivity(intent);
     }
 }
